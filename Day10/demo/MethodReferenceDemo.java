a146fe46b4eaf2f81e9baa89eab5bb697fa57b2f
KrishYASWANT <yaswantkrish321@gmail.com>
2022-02-10 19:40:41 +0530

    lambda expression and references

---
+++ Day10/demo/MethodReferenceDemo.java
@@ -0,0 +1,35 @@
+package Day10.demo;
+
+@FunctionalInterface
+interface Messagable {
+
+	void getMessage(String msg);
+
+}
+
+class Message {
+
+	void Show(String msg) {
+
+		System.out.println(" from method");
+		System.out.println(" =====================" + msg);// using method here
+	}
+
+}
+
+public class MethodReferenceDemo {
+
+	public static void main(String[] args) {
+
+		Message m = new Message();// creating obj
+
+		// interface
+		Messagable reference = m::Show; // reference of interface here
+
+		// calling here
+
+		reference.getMessage("Hi");
+
+	}
+
+}
