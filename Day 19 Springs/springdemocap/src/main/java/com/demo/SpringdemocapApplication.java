095112a636b32f8c6bdd55058542000ac622fd3f
KrishYASWANT <yaswantkrish321@gmail.com>
2022-03-03 16:51:20 +0530

    Spring Concepts

---
+++ Day 19 Springs/springdemocap/src/main/java/com/demo/SpringdemocapApplication.java
@@ -0,0 +1,14 @@
+package com.demo;
+
+/*
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+@SpringBootApplication
+public class SpringdemocapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringdemocapApplication.class, args);
+	}
+
+}*/
